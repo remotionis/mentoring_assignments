d431fa9886eca50ae592c3431c3fd0340f681f22
remotionis <remotion371@soongsil.ac.kr>
2021-02-19 14:48:44 +0900

    Add files

---
+++ 3주차/4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class Calculator {
+	private:
+		int Cplus;
+		int Cminus;
+		int Cmul;
+		int Cdiv;
+	public:
+		double Plus(double num1, double num2);
+		double Minus(double num1, double num2);
+		double Mul(double num1, double num2);
+		double Div(double num1, double num2);
+}
+
+int main()
+{
+	Calculator Cal;
+	
+
+	return 0;
+}
